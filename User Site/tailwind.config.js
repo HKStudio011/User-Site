09fbaf5e852fe40bc14b3fdf730c1bd8076d7a1d
H.K <trucngonghinh2002@gmail.com>
2025-10-02 23:50:46 +0700

    tich hop tailwind

---
+++ User Site/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './Pages/**/*.razor.css',
+    './Pages/**/*.razor.css',
+    './Components/**/*.razor.css',
+    './Components/**/*.razor.css',
+    './Shared/**/*.razor.css',
+    './Shared/**/*.razor.css',
+    './wwwroot/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
